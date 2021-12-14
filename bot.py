e604686c28226a0e069244ae27e2b74b2560d1d6
lht99 <33472127+lht99@users.noreply.github.com>
2021-12-14 17:40:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,8 @@ def main():
 
     # log all errors
     dp.add_error_handler(error)
-    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN, webhook_url="https://sci-letuan.herokuapp.com/" + TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN, webhook_url="https://yourheroku.herokuapp.com/" + TOKEN)
+    #Thay thế {yourheroku} bằng heroku của bạn
     updater.idle()
 
 
